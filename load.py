675c6ea7bb349b9466c7fd6fbbbf61a70d8d74ba
FirelightFlagboy <firelight.flagboy@gmail.com>
2018-06-03 18:14:40 +0200

    convert json type array to numpy version

--- load.py
+++ load.py
@@ -4,11 +4,17 @@ import numpy as np
 
 def loadWeigth():
 	with open(const.FILE_WEIGTH, 'r') as file:
-		return json.loads(file.read())
+		weigth = json.loads(file.read())
+		for key in weigth.keys():
+			weigth[key] = np.matrix(weigth[key])
+		return weigth
 
 def loadTraining():
 	with open(const.FILE_TRAINING, 'r') as file:
-		return json.loads(file.read())
+		training = json.loads(file.read())
+		for i, data in enumerate(training):
+			training[i]['input'] = np.array(data['input'])
+		return training
 
 def main():
 	weigth = loadWeigth()
